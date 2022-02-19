e6e314bb76d5c2997ad622909d69ae4cbaffc00d
Damian Makki <damian@makki.pro>
2022-02-19 17:52:54 -0600

    add fadeInDown animation to <section> elements

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,30 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
+var observer = new IntersectionObserver(changes => {
+  changes.forEach(change => {
+    change.isIntersecting && change.target.classList.add('fadeInDown');
+  });
+}, {});
+
+
+function addAnimation(element) {
+  let animatedItems = document.querySelectorAll(element);
+  animatedItems.forEach(item => {
+    observer.observe(item);
+  })
+}
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
+
+  useEffect(() => {
+    addAnimation('section');
+  }, []);
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
