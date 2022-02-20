b00b88737bbb4cb39f33db95d2941cec42b26fdb
Damian Makki <damian@makki.pro>
2022-02-19 18:06:50 -0600

    update observer in useEffect

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,25 +4,27 @@ import { Link } from "gatsby"
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
-var observer = new IntersectionObserver(changes => {
-  changes.forEach(change => {
-    change.isIntersecting && change.target.classList.add('fadeInDown');
-  });
-}, {});
 
-
-function addAnimation(element) {
-  let animatedItems = document.querySelectorAll(element);
-  animatedItems.forEach(item => {
-    observer.observe(item);
-  })
-}
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
 
   useEffect(() => {
+
+    var observer = new IntersectionObserver(changes => {
+      changes.forEach(change => {
+        change.isIntersecting && change.target.classList.add('fadeInDown');
+      });
+    }, {});
+
+    function addAnimation(element) {
+      let animatedItems = document.querySelectorAll(element);
+      animatedItems.forEach(item => {
+        observer.observe(item);
+      })
+    }
+
     addAnimation('section');
   }, []);
 
